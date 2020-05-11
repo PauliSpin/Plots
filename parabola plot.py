932eec06e65eee36ff665d47660968adef33bbd2
Chotalia <rchotalia@dxc.com>
2020-05-11 13:30:49 +0100

    Only plot if roots are Real

--- parabola plot.py
+++ parabola plot.py
@@ -8,44 +8,47 @@ def main():
 
     # Define parabola with real roots
     p_real = prb.Parabola(1, -7, 10)
+
     print(p_real)
     print(f'Roots are {p_real.root_type}')
     print(f'\troot1 = {p_real.root1}')
     print(f'\troot2 = {p_real.root2}')
     print()
 
-    # Define number of points to be plotted
-    N = 200
-    # Set the x-distance between the two roots
-    dist_root1_root2 = abs(p_real.root2 - p_real.root1)
-    # Define the x-lower and x-upper limits to be a multiple of the
-    # distance between the roots
-    x_lower = p_real.root1 - 2 * dist_root1_root2
-    x_upper = p_real.root2 + 2 * dist_root1_root2
+    if p_real.root_type == "Real":
 
-    step = (x_upper - x_lower) / N
+        # Define number of points to be plotted
+        N = 200
+        # Set the x-distance between the two roots
+        dist_root1_root2 = abs(p_real.root2 - p_real.root1)
+        # Define the x-lower and x-upper limits to be a multiple of the
+        # distance between the roots
+        x_lower = p_real.root1 - 2 * dist_root1_root2
+        x_upper = p_real.root2 + 2 * dist_root1_root2
 
-    x = []
-    y = []
+        step = (x_upper - x_lower) / N
 
-    for i in range(0, N):
-        x.append(x_lower + i * step)
-        y.append(p_real.y(x_lower + i * step))
+        x = []
+        y = []
 
-    # print(x)
-    # print(y)
+        for i in range(0, N):
+            x.append(x_lower + i * step)
+            y.append(p_real.y(x_lower + i * step))
 
-    # Set the plot style
-    plt.style.use('fivethirtyeight')
+        # print(x)
+        # print(y)
 
-    # Set plot title, x and y labels
-    plt.title(p_real)
-    plt.xlabel('x')
-    plt.ylabel('y')
-    plt.plot(x, y, label='y(x)', linewidth=1)
-    plt.tight_layout()
+        # Set the plot style
+        plt.style.use('fivethirtyeight')
 
-    plt.show()
+        # Set plot title, x and y labels
+        plt.title(p_real)
+        plt.xlabel('x')
+        plt.ylabel('y')
+        plt.plot(x, y, label='y(x)', linewidth=1)
+        plt.tight_layout()
+
+        plt.show()
 
 
 if __name__ == '__main__':
